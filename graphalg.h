7bf02cc707fa19415c027c90783b6973ae71903d
DonKidor <JIaIILLIa3@yandex.ru>
2021-03-13 00:07:22 +0700

    Add header file

---
+++ graphalg.h
@@ -0,0 +1,17 @@
+#include <vector>
+
+
+struct response {
+	int code;
+	int answer;
+	char* message;
+};
+
+response calcCountPaths(std::vector<std::vector<int> > g, int n, int fromNode, int toNode);
+response calcShortestPath(std::vector<std::vector<int> > g, int n, int fromNode, int toNode);
+response calcLongestPath(std::vector<std::vector<int> > g, int n, int fromNode, int toNode);
+
+//g - info about edges of graph
+//n - count of vertices
+//fromNode - index of Node, where start calculations
+//toNode - index of Node, where end calculations
