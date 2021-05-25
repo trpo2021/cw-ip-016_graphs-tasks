007609faa606591567f1d76abfec24bc53a3ab7c
DonKidor <JIaIILLIa3@yandex.ru>
2021-05-25 13:30:44 +0700

    Write tests for arguments main function

--- src/test/tests.cpp
+++ src/test/tests.cpp
@@ -41,10 +41,82 @@ CTEST2(suite, test1) {
 
 CTEST2(suite, test2) {
 	data->f.good();
-	int a;
-	std::cin >> a;
-	CTEST_LOG("%d",a);
-	ASSERT_STR("foo", "foo");
+	const char *args[] = {mPath.c_str(),"4","5","-m"};
+	const std::string result="no argument for -m\n";
+	nmain(4,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(suite, test3) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"4","5","-m","bb"};
+	const std::string result="invalid argument for -m, expected [sp, lp, cp], found bb\n";
+	nmain(5,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(suite, test4) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"4","5","-i"};
+	const std::string result="no argument for -i\n";
+	nmain(4,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(suite, test5) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"4","5","-o"};
+	const std::string result="no argument for -o\n";
+	nmain(4,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(suite, test6) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"4","5","6"};
+	const std::string result="too much arguments (6)\n";
+	nmain(4,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(suite, test7) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"4","5","boba"};
+	const std::string result="what is boba?\n";
+	nmain(4,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(suite, test8) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"4","5","-i","not/found/file.txt"};
+	const std::string result="File not found\n";
+	nmain(5,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(suite, test9) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"4","-m","sp"};
+	const std::string result="no argument for begin node or end node\n";
+	nmain(4,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
 }
 
 
