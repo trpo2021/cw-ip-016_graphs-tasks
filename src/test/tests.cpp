5fa8b6effa0b44bf8793d4dddb0a392642d43136
DonKidor <JIaIILLIa3@yandex.ru>
2021-05-26 20:11:20 +0700

    Write tests for all aruments and errors

--- src/test/tests.cpp
+++ src/test/tests.cpp
@@ -1,8 +1,11 @@
 #include <test/ctest.h>
+#include <graph/graphalg.h>
 
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <cstdio>
+#include <vector>
 #include <string>
 
 extern std::string mPath;
@@ -118,6 +121,96 @@ CTEST2(suite, test9) {
 	CTEST_LOG("%s",a.c_str());
 	ASSERT_STR(result.c_str(), a.c_str());
 }
+
+CTEST2(suite, test10) {
+	data->f.good();
+	const char *args[] = {mPath.c_str(),"1","20"};
+	const std::string result="begin node or end node is not in range\n";
+	nmain(3,args);
+	std::string a=data->ss.str();
+	CTEST_LOG("%s",a.c_str());
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+
+
+
+CTEST(cycle, test1) {
+	std::string p=mPath+"tests/cycle.test";
+	std::string po=mPath+"tests/out";
+	const char *args[] = {mPath.c_str(),"1","6","-m","cp","-i",p.c_str(),"-o",po.c_str()};
+	nmain(9,args);
+	std::ifstream f(po);
+	std::string result="loop";
+	std::string a="";
+	if(f.is_open()) {
+		f >> a;
+		CTEST_LOG("%s",a.c_str());
+		f.close();
+		std::remove(po.c_str());
+	}
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST_DATA(serr) {
+	std::stringstream ss;
+};
+
+CTEST_SETUP(serr) {
+	std::cout.rdbuf(data->ss.rdbuf());
+}
+
+CTEST_TEARDOWN(serr) {
+	std::cout.rdbuf(data->ss.rdbuf());
+}
+
+CTEST2(serr, test1) {
+	std::string p=mPath+"tests/incorrect.test";
+	
+	const char *args[] = {mPath.c_str(),"1","4","-m","cp","-i",p.c_str()};
+	nmain(7,args);
+	std::string result="information about edges is iscorrect\n";
+	std::string a="";
+	a=data->ss.str();
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(serr, test2) {
+	std::string p=mPath+"tests/incorvert.test";
+	
+	const char *args[] = {mPath.c_str(),"1","4","-m","cp","-i",p.c_str()};
+	nmain(7,args);
+	std::string result="count of verticies and edge must be number\n";
+	std::string a="";
+	a=data->ss.str();
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+
+CTEST2(serr, test3) {
+	std::string p=mPath+"tests/incoredge.test";
+	
+	const char *args[] = {mPath.c_str(),"1","4","-m","cp","-i",p.c_str()};
+	nmain(7,args);
+	std::string result="parameters of each edge must be follow this pattern: [(from "
+	              "verticy) (to verticy) (weight)] all must be number\n";
+	std::string a="";
+	a=data->ss.str();
+	ASSERT_STR(result.c_str(), a.c_str());
+}
+CTEST(calc, shortest) {
+	const int n=4;
+	std::vector<std::vector<int> > g = {
+	{-1,3,2,-1},
+	{-1,-1,2,-1},
+	{-1,-1,-1,1},
+	{-1,1,-1,-1}
+	};
+	response res=calcShortestPath(g,n,0,3);
+	ASSERT_EQUAL(res.code, 0);
+	ASSERT_EQUAL(res.answer, 3);
+}
+
+
 
 
 
